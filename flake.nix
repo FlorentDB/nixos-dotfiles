2584016a4725084187f99b604f7f7ef2fc4910dc
florent <florent.donat.bouillud@gmail.com>
2026-02-28 21:19:12 +0100

    refactor: restructure dotfiles (system/, modules/)

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Hyprland the 1st";
+  description = "Beginner config with noctalia-shell";
   
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -9,25 +9,31 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    noctalia = {
+   noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.noctalia-qs.follows = "noctalia-qs";
     };
-  };
+
+    noctalia-qs = {
+      url = "github:noctalia-dev/noctalia-qs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }; 
+};
 
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
     specialArgs = {inherit inputs;};
       modules = [
-        ./configuration.nix
+        ./system/configuration.nix
         home-manager.nixosModules.home-manager
         {
           home-manager = {
             useGlobalPkgs = true;
             useUserPackages = true;
 	    extraSpecialArgs = { inherit inputs; };
-            users.florent = import ./home.nix;
+            users.florent = import ./modules/home.nix;
             backupFileExtension = "backup";
           };
         } 
